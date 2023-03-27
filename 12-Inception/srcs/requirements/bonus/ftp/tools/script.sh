feae7ffd5fbfb777bcf410fcd4b70476fb25320c
ripa <davide.ripanucci@gmail.com>
2023-03-27 15:35:45 +0200

    Inception

---
+++ 12-Inception/srcs/requirements/bonus/ftp/tools/script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+service vsftpd start
+
+# Add the USER, change his password and declare him as the owner of wordpress folder and all subfolders
+
+adduser $ftp_user --disabled-password
+
+echo "$ftp_user:$ftp_pwd" | /usr/sbin/chpasswd &> /dev/null
+
+echo "$ftp_user" | tee -a /etc/vsftpd.userlist &> /dev/null
+
+mkdir /home/$ftp_user/ftp
+
+chown nobody:nogroup /home/$ftp_user/ftp
+chmod a-w /home/$ftp_user/ftp
+
+mkdir /home/$ftp_user/ftp/files
+chown $ftp_user:$ftp_user /home/$ftp_user/ftp/files
+
+sed -i -r "s/#write_enable=YES/write_enable=YES/1"   /etc/vsftpd.conf
+sed -i -r "s/#chroot_local_user=YES/chroot_local_user=YES/1"   /etc/vsftpd.conf
+
+echo "
+local_enable=YES
+allow_writeable_chroot=YES
+pasv_enable=YES
+local_root=/home/sami/ftp
+pasv_min_port=40000
+pasv_max_port=40005
+userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf
+
+service vsftpd stop
+
+/usr/sbin/vsftpd
---
+++ 12-Inception/srcs/requirements/nginx/tools/script.sh
@@ -0,0 +1,26 @@
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=IT/ST=Italy/L=Rome/O=42/OU=42/CN=[dripanuc.42.fr](http://dripanuc.42.fr/)"
+
+echo "
+server { 
+    listen 443 ssl;
+    listen [::]:443 ssl;
+
+    #server_name www.$DOMAIN_NAME $DOMAIN_NAME;
+
+    ssl_certificate $CERTS_;
+    ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;" > /etc/nginx/sites-available/default
+
+
+echo
+	'ssl_protocols TLSv1.3;
+    index index.php;
+    root /var/www/html;
+    location ~ [^/]\.php(/|$) {
+            try_files $uri =404;
+            fastcgi_pass wordpress:9000;
+            include fastcgi_params;
+            fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        }
+} ' >>  /etc/nginx/sites-available/default
+
+nginx -g "daemon off;"
