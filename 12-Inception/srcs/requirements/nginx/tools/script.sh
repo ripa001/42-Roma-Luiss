87ef4f76eaee2bb5ab0ddfbc1185d90bdc93ca09
ripa <davide.ripanucci@gmail.com>
2023-04-04 01:06:28 +0200

    Inception

--- 12-Inception/srcs/requirements/nginx/tools/script.sh
+++ 12-Inception/srcs/requirements/nginx/tools/script.sh
@@ -2,30 +2,30 @@
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/dripanuc.key -out $CERTS_ -subj "/C=IT/ST=Italy/L=Rome/O=42/OU=42/CN=dripanuc"
 
-echo "
-server {
-    listen 443 ssl;
-    listen [::]:443 ssl;
+# echo "
+# server {
+#     listen 443 ssl;
+#     listen [::]:443 ssl;
 
-    #server_name www.$DOMAIN_NAME $DOMAIN_NAME;
+#     #server_name www.$DOMAIN_NAME $DOMAIN_NAME;
 
-    ssl_certificate $CERTS_;
-    ssl_certificate_key /etc/ssl/private/dripanuc.key;" > /etc/nginx/sites-available/default
+#     ssl_certificate $CERTS_;
+#     ssl_certificate_key /etc/ssl/private/dripanuc.key;" > /etc/nginx/sites-available/default
 
 
-echo '
-    ssl_protocols TLSv1.3;
+# echo '
+#     ssl_protocols TLSv1.3;
 
-    index index.php;
-    root /var/www/html;
+#     index index.php;
+#     root /var/www/html;
 
-    location ~ [^/]\.php(/|$) { 
-            try_files $uri =404;
-            fastcgi_pass wordpress:9000;
-            include fastcgi_params;
-            fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        }
-} ' >>  /etc/nginx/sites-available/default
+#     location ~ [^/]\.php(/|$) { 
+#             try_files $uri =404;
+#             fastcgi_pass wordpress:9000;
+#             include fastcgi_params;
+#             fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+#         }
+# } ' >>  /etc/nginx/sites-available/default
 
 
 nginx -g "daemon off;"
