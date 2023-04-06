108e267f002825160660093e9378bf6000cfcb01
ripa <davide.ripanucci@gmail.com>
2023-04-06 12:53:37 +0200

    Inception

--- 12-Inception/srcs/requirements/mariadb/tools/script.sh
+++ 12-Inception/srcs/requirements/mariadb/tools/script.sh
@@ -4,11 +4,11 @@ service mysql start
 
 echo $DB1_PWD $DB1_USER $DB1_NAME
 
-echo "CREATE DATABASE IF NOT EXISTS $DB1_NAME ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$DB1_USER'@'%' IDENTIFIED BY '$DB1_PWD' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $DB1_NAME.* TO '$DB1_USER'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
+echo 'CREATE DATABASE IF NOT EXISTS $DB1_NAME ;' > db1.sql
+echo 'CREATE USER IF NOT EXISTS "$DB1_USER"@"%" IDENTIFIED BY "$DB1_PWD" ;' >> db1.sql
+echo 'GRANT ALL PRIVILEGES ON $DB1_NAME.* TO "$DB1_USER"@"%" ;' >> db1.sql
+echo 'ALTER USER "root"@"localhost" IDENTIFIED BY "12345" ;' >> db1.sql
+echo 'FLUSH PRIVILEGES;' >> db1.sql
 
 mysql < db1.sql
 
