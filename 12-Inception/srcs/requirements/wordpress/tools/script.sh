154644299bb009e0d59a0bbef02210e08e532ed7
ripa <davide.ripanucci@gmail.com>
2023-04-21 01:38:27 +0200

    Inception

--- 12-Inception/srcs/requirements/wordpress/tools/script.sh
+++ 12-Inception/srcs/requirements/wordpress/tools/script.sh
@@ -26,16 +26,19 @@ then
 	####### BONUS PART ################
 
 	## redis ##
+	echo "REDIS START STATUS : OK"
+	wp config set WP_REDIS_HOST redis --allow-root #I put --allowroot because i am on the root user on my VM
+	wp config set WP_REDIS_PORT 6379 --raw --allow-root
+	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
+	#wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
+	wp config set WP_REDIS_CLIENT phpredis --allow-root
+	echo "REDIS CONFIG STATUS : OK"
+	wp plugin install redis-cache --activate --allow-root
+	wp plugin update --all --allow-root
+	echo "REDIS PLUGIN STATUS : OK"
+	wp redis enable --allow-root
 
-		wp config set WP_REDIS_HOST redis --allow-root #I put --allowroot because i am on the root user on my VM
-		wp config set WP_REDIS_PORT 6379 --raw --allow-root
-		wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
-		#wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
-		wp config set WP_REDIS_CLIENT phpredis --allow-root
-		wp plugin install redis-cache --activate --allow-root
-		wp plugin update --all --allow-root
-		wp redis enable --allow-root
-
+	echo "REDIS STATUS : OK"
 	###  end of redis part  ###
 
 fi
