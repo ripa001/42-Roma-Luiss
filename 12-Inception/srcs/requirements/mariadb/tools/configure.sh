2803d229eaf1b80e78929d89fe37f71e0dc954c4
ripa <davide.ripanucci@gmail.com>
2023-04-20 21:18:22 +0200

    Mandatory Inception finish

---
+++ 12-Inception/srcs/requirements/mariadb/tools/configure.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+if [ ! -d "/run/mysqld" ];
+then
+    mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld
+fi
+
+if [ ! -d "/var/lib/mysql/mysql" ];
+then
+    chown -R mysql:mysql /var/lib/mysql
+    
+    mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+
+    tmp=`mktemp`
+    if [ ! -f "$tmp" ];
+    then
+        return 1
+    fi
+    cat << EOF > $tmp
+
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '12345';
+CREATE DATABASE $DB1_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$DB1_USER'@'%' IDENTIFIED BY '$DB1_PWD';
+GRANT ALL PRIVILEGES ON $DB1_NAME.* TO '$DB1_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+    /usr/bin/mysqld --user=mysql --bootstrap < $tmp
+    rm -f $tmp 
+fi
+
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+
+echo "MariaDB starting: OK"
+
+exec /usr/bin/mysqld --user=mysql --console
--- 12-Inception/srcs/requirements/wordpress/tools/script.sh
+++ 12-Inception/srcs/requirements/wordpress/tools/script.sh
@@ -1,52 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 
-# create directory to use in nginx container later and also to setup the wordpress conf
-mkdir /var/www/
-mkdir /var/www/html
+#wait for mariadb, then connect with credentials
 
-cd /var/www/html
+while ! mariadb -h$DB_HOSTNAME -u$DB_USER -p$DB_PWD $DB_NAME &>/dev/null;
+do
+    sleep 3
+    echo "waiting..."
+	echo "Host: $DB_HOSTNAME\n\
+			User: $DB_USER\n\
+			Pwd: $DB_PWD\n\
+			Name: $DB_NAME\n " 
+done
 
-# remove all the wordpress files if there is something from the volumes to install it again
-rm -rf *
+if [ ! -f "/var/www/html/wordpress/index.php" ];
+then
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+	mv /tmp/index.html /var/www/html/wordpress/index.html
 
-# makes the WP-CLI PHAR file executable.
-chmod +x wp-cli.phar
+	wp core download --allow-root
+	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbhost=$DB_HOSTNAME --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+	wp db create --allow-root
+	wp core install --url=$DOMAIN_NAME --title="Inception" --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+	wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+	wp theme activate twentytwentythree --allow-root
 
-# moves the WP-CLI PHAR file to the /usr/local/bin directory, which is in the system's PATH, and renames it to wp. This allows you to run the wp command from any directory
-mv wp-cli.phar /usr/local/bin/wp
+fi
 
-# downloads the latest version of WordPress to the current directory. The --allow-root flag allows the command to be run as the root user, which is necessary if you are logged in as the root user or if you are using WP-CLI with a system-level installation of WordPress.
-wp core download --allow-root
+echo "WORDPRESS START STATUS : OK"
 
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
-
-sed -i -r "s/db1/$DB_NAME/1"   wp-config.php
-sed -i -r "s/user/$DB_USER/1"  wp-config.php
-sed -i -r "s/pwd/$DB_PWD/1"    wp-config.php
-
-# BASIC WORDPRESS SETUP
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-
-wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-
-
-wp theme install astra --activate --allow-root
-
-
-wp plugin install redis-cache --activate --allow-root
-
-wp plugin update --all --allow-root
-
-
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-
-mkdir /run/php
-
-
-
-wp redis enable --allow-root
-
-/usr/sbin/php-fpm7.3 -F
+/usr/sbin/php-fpm7 -F -R
