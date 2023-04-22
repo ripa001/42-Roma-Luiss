b4d3e5b700ad50c6a131bcd66676458f51b6ddf8
ripa <davide.ripanucci@gmail.com>
2023-04-23 00:33:47 +0200

    Inception

--- 12-Inception/srcs/requirements/wordpress/tools/script.sh
+++ 12-Inception/srcs/requirements/wordpress/tools/script.sh
@@ -30,7 +30,7 @@ then
 	wp config set WP_REDIS_HOST redis --allow-root #I put --allowroot because i am on the root user on my VM
 	wp config set WP_REDIS_PORT 6379 --raw --allow-root
 	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
-	wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
+	# wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
 	wp config set WP_REDIS_CLIENT phpredis --allow-root
 	echo "REDIS CONFIG STATUS : OK"
 	wp plugin install redis-cache --activate --allow-root
