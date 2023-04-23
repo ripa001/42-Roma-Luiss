911830c940cab83a9a3dfac4c9af4780b2eefe22
ripa <davide.ripanucci@gmail.com>
2023-04-23 12:59:24 +0200

    Inception

--- 12-Inception/srcs/requirements/wordpress/tools/script.sh
+++ 12-Inception/srcs/requirements/wordpress/tools/script.sh
@@ -27,7 +27,7 @@ then
 
 	## redis ##
 	echo "REDIS START STATUS : OK"
-	wp config set WP_REDIS_HOST 0.0.0.0 --allow-root #I put --allowroot because i am on the root user on my VM
+	wp config set WP_REDIS_HOST redis --allow-root #I put --allowroot because i am on the root user on my VM
 	wp config set WP_REDIS_PORT 6379 --raw --allow-root
 	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
 	wp config set WP_CACHE true --raw --type=constant --allow-root
